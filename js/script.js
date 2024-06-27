203b7c6ab84838ace9b754a9fc597944fedd8409
gouribhise <gouri.a.bhise@gmail.com>
2024-06-27 19:20:00 +0530

    fixing index

--- js/script.js
+++ js/script.js
@@ -12,73 +12,72 @@ var frontw = document.getElementById("front-projects");
 var fulls=document.getElementById('full-projects')
 var mobile=document.getElementById('android-projects')
 var design=document.getElementById('graphics-projects')
- function index_start(){
 
-  fulls.style.display='none';
+ 
+ function consoleText(words, id, colors) {
+  console.log('inside consoletext')
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
+
+function index_start(){
+console.log('inside index_start')
+  fulls.style.display ="none"
   mobile.style.display="none";
   design.style.display="none"
-  consoleText(['Full Stack Web Developer', 'Android Developer', 'Graphic Designer','Transcriptionist'], 'text',['lightblue','tomato','rebeccapurple']);
   
- function consoleText(words, id, colors) {
-   if (colors === undefined) colors = ['#fff'];
-   var visible = true;
-   var con = document.getElementById('console');
-   var letterCount = 1;
-   var x = 1;
-   var waiting = false;
-   var target = document.getElementById(id)
-   target.setAttribute('style', 'color:' + colors[0])
-   window.setInterval(function() {
- 
-     if (letterCount === 0 && waiting === false) {
-       waiting = true;
-       target.innerHTML = words[0].substring(0, letterCount)
-       window.setTimeout(function() {
-         var usedColor = colors.shift();
-         colors.push(usedColor);
-         var usedWord = words.shift();
-         words.push(usedWord);
-         x = 1;
-         target.setAttribute('style', 'color:' + colors[0])
-         letterCount += x;
-         waiting = false;
-       }, 1000)
-     } else if (letterCount === words[0].length + 1 && waiting === false) {
-       waiting = true;
-       window.setTimeout(function() {
-         x = -1;
-         letterCount += x;
-         waiting = false;
-       }, 1000)
-     } else if (waiting === false) {
-       target.innerHTML = words[0].substring(0, letterCount)
-       letterCount += x;
-     }
-   }, 120)
-   window.setInterval(function() {
-     if (visible === true) {
-       con.className = 'console-underscore hidden'
-       visible = false;
- 
-     } else {
-       con.className = 'console-underscore'
- 
-       visible = true;
-     }
-   }, 400)
+
  }
- 
- }
- 
  //end of index
 // Add active class to the current button (highlight it)
 
 
 
-
-function start(){
-  
-}
+ 
  
 function front(){
    console.log('front called')
